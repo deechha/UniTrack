5ed6bafeec8d47628f2292cddb73ad7a60439af9
deechha <deepachapagain110@gmail.com>
2025-07-16 21:26:06 +0545

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import StudentDashboard from "./components/StudentDashboard";
-import AdminDashboard from "./components/AdminDashboard";
-import CoordinatorDashboard from "./components/CoordinatorDashboard";
+import Login from "./pages/Login";
+import StudentDashboard from "./pages/StudentDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import CoordinatorDashboard from "./pages/CoordinatorDashboard";
+import HomePage from "./pages/HomePage";
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/student-dashboard" element={<StudentDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/coordinator-dashboard" element={<CoordinatorDashboard />} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/student-dashboard" element={<StudentDashboard/>} />
+        <Route path="/admin-dashboard" element={<AdminDashboard/>} />
+        <Route path="/coordinator-dashboard" element={<CoordinatorDashboard/>} />
       </Routes>
     </Router>
   );
